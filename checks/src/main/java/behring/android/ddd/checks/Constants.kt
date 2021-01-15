57f70fbfd48ea6129f27bd1b8fa36195830488f2
zhaolin <lizhao@thoughtworks.com>
2021-01-15 17:58:21 +0800

    常量添加const

--- checks/src/main/java/behring/android/ddd/checks/Constants.kt
+++ checks/src/main/java/behring/android/ddd/checks/Constants.kt
@@ -1,6 +1,6 @@
 package behring.android.ddd.checks
 
 const val PACKAGE_NAME: String = "behring.android.ddd"
-val PACKAGE_NAME_DOMAIN_LAYER: String = "$PACKAGE_NAME.domain"
-val PACKAGE_NAME_DATA_LAYER: String = "$PACKAGE_NAME.data"
-val PACKAGE_NAME_ANDROID_LAYER: String = "$PACKAGE_NAME.android"
+const val PACKAGE_NAME_DOMAIN_LAYER: String = "$PACKAGE_NAME.domain"
+const val PACKAGE_NAME_DATA_LAYER: String = "$PACKAGE_NAME.data"
+const val PACKAGE_NAME_ANDROID_LAYER: String = "$PACKAGE_NAME.android"
